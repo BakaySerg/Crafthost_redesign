13a5cdbfe168edcb78b0151ab80cd24ac81a582c
Bakay Serg <bakay.sergey28@gmail.com>
2026-04-25 21:39:06 +0300

    about-case-studies - begin // gulp update (+ img-min & gulp-newer)

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,33 @@
 const gulp = require("gulp"),
 	sourcemaps = require("gulp-sourcemaps"),
 	sass = require("gulp-sass")(require("sass")),
-	browserSync = require("browser-sync"),
-	// concat        = require('gulp-concat'),
-	// uglify        = require('gulp-uglify'),
+	browserSync = require("browser-sync").create(),
 	changed = require("gulp-changed"),
 	cleancss = require("gulp-clean-css"),
-	rename = require("gulp-rename"),
 	fileinclude = require("gulp-file-include"),
 	svgSprite = require("gulp-svg-sprite"),
 	path = require("path"),
 	glob = require("glob"),
 	replace = require("gulp-replace"),
-	// groupMedia        = require('gulp-group-css-media-queries'), //"gulp-group-css-media-queries": "^1.2.2",
-	notify = require("gulp-notify");
+	imagemin = require("gulp-imagemin"),
+	newer = require("gulp-newer");
 
+// BrowserSync
 gulp.task("browser-sync", function (done) {
-	browserSync({
+	browserSync.init({
 		server: { baseDir: "app" },
 		notify: false,
-		// open: false,
-		// online: false, // Work Offline Without Internet Connection
-		// tunnel: true, tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
 	});
 	done();
 });
 
+// SVG sprite (only SVG)
 gulp.task("svg-sprite", function (done) {
 	const folders = glob.sync("dev/img/**/");
 	const tasks = folders.map((folder) => {
 		const hasSvg = glob.sync(path.join(folder, "*.svg")).length > 0;
 		if (!hasSvg) return Promise.resolve();
 
-		// Вычисляем относительный путь от корня картинок
 		const relativePath = path.relative("dev/img", folder);
 
 		const config = {
@@ -56,22 +51,30 @@ gulp.task("svg-sprite", function (done) {
 			},
 		};
 
-		return (
-			gulp
-				.src(path.join(folder, "*.svg"))
-				.pipe(replace(/fill\s*=\s*"(?!\s*(?:none|url\(|[a-zA-Z]+))[^"]*"/gi, 'fill="currentColor"'))
-				.pipe(replace(/stroke\s*=\s*"(?!\s*(?:none|url\(|[a-zA-Z]+))[^"]*"/gi, 'stroke="currentColor"'))
-				.pipe(svgSprite(config))
-				.pipe(gulp.dest(path.join("app/img", relativePath)))
-		);
+		return gulp
+			.src(path.join(folder, "*.svg"))
+			.pipe(replace(/fill\s*=\s*"(?!\s*(?:none|url\(|[a-zA-Z]+))[^"]*"/gi, 'fill="currentColor"'))
+			.pipe(replace(/stroke\s*=\s*"(?!\s*(?:none|url\(|[a-zA-Z]+))[^"]*"/gi, 'stroke="currentColor"'))
+			.pipe(svgSprite(config))
+			.pipe(gulp.dest(path.join("app/img", relativePath)));
 	});
 
 	return Promise.all(tasks).then(() => done());
 });
 
+// Images (jpg, png, etc. without SVG)
+gulp.task("images", function () {
+	return gulp
+		.src(["dev/img/**/*", "!dev/img/**/*.svg"])
+		.pipe(newer("app/img"))
+		.pipe(imagemin([imagemin.mozjpeg({ quality: 75, progressive: true }), imagemin.optipng({ optimizationLevel: 5 })]))
+		.pipe(gulp.dest("app/img"))
+		.pipe(browserSync.stream());
+});
 
-gulp.task('layout', function() {
-   return gulp
+// HTML
+gulp.task("layout", function () {
+	return gulp
 		.src("dev/*.html")
 		.pipe(
 			fileinclude({
@@ -79,58 +82,37 @@ gulp.task('layout', function() {
 				basepath: "@file",
 			}),
 		)
-		.pipe(changed("app", { hasChanged: changed.compareContents })) // Ignore unchanged files (after all @@including)
+		.pipe(changed("app", { hasChanged: changed.compareContents }))
 		.pipe(gulp.dest("app/"))
-		.pipe(browserSync.reload({ stream: true }));
+		.pipe(browserSync.stream());
 });
 
-gulp.task('styles', function() {
-	return (
-		gulp
-			.src("dev/scss/**/*.scss")
-			// return gulp.src('dev/scss/*.scss')
-			.pipe(sourcemaps.init({ loadMaps: true }))
-			.pipe(sass({ outputStyle: "expanded", silenceDeprecations: ["legacy-js-api"] }).on("error", sass.logError))
-			// .pipe(rename({ suffix: '.min', prefix : '' }))
-			// .pipe(groupMedia())             // before build project for production
-			.pipe(cleancss({ level: { 1: { specialComments: 0 } } }))
-			.pipe(sourcemaps.write("./"))
-			.pipe(gulp.dest("./app/css/"))
-			.pipe(browserSync.reload({ stream: true }))
-	);
-});
-// gulp.task('styles-separated', function() {
-// 	return gulp.src('dev/scss/pages/*.scss')   	//pages
-// 	.pipe(sourcemaps.init({loadMaps: true}))
-// 	.pipe(sass({ outputStyle: 'expanded' }).on("error", notify.onError()))
-// 	.pipe(rename({ suffix: '.min', prefix : '' }))
-// 	.pipe(cleancss( {level: { 1: { specialComments: 0 }}}))
-// 	.pipe(gulp.dest('app/css'))
-// 	.pipe(sourcemaps.write('./'))
-// 	.pipe(gulp.dest('./app/css/'))
-// 	.pipe(browserSync.reload({ stream: true }));
-// });
-
-gulp.task('scripts', function() {
-	return gulp.src([
-		'dev/js/*.js', // Always (scripts) at the end
-		])
-	// .pipe(concat('scripts.min.js'))
-	// .pipe(uglify()) // Minify js - opt.
-	.pipe(gulp.dest('app/js'))
-	.pipe(browserSync.reload({ stream: true }))
+// Styles
+gulp.task("styles", function () {
+	return gulp
+		.src("dev/scss/**/*.scss")
+		.pipe(sourcemaps.init({ loadMaps: true }))
+		.pipe(sass({ outputStyle: "expanded", silenceDeprecations: ["legacy-js-api"] }).on("error", sass.logError))
+		.pipe(cleancss({ level: { 1: { specialComments: 0 } } }))
+		.pipe(sourcemaps.write("./"))
+		.pipe(gulp.dest("./app/css/"))
+		.pipe(browserSync.stream());
 });
 
-gulp.task('code', function() {
-	return gulp.src('app/*.html')
-	.pipe(browserSync.reload({ stream: true }))
+// Scripts
+gulp.task("scripts", function () {
+	return gulp.src("dev/js/*.js").pipe(gulp.dest("app/js")).pipe(browserSync.stream());
 });
 
-
+// Watch
 gulp.task("watch", function (done) {
-	gulp.watch(["dev/**/*.js", "dev/js/main.js"], gulp.parallel("scripts"));
+	gulp.watch(["dev/**/*.js"], gulp.parallel("scripts"));
 	gulp.watch("dev/**/*.html", gulp.parallel("layout"));
 	gulp.watch("dev/scss/**/*.scss", gulp.parallel("styles"));
+	gulp.watch("dev/img/**/*.svg", gulp.parallel("svg-sprite"));
+	gulp.watch(["dev/img/**/*", "!dev/img/**/*.svg"], gulp.parallel("images"));
 	done();
 });
-gulp.task('default', gulp.parallel('layout','styles', 'scripts', 'browser-sync', 'watch'));
+
+// Default
+gulp.task("default", gulp.parallel("layout", "styles", "scripts", "images", "svg-sprite", "browser-sync", "watch"));
